55b1f4f30d7fe1476bd966a5dc95c7eceed00ae1
Ikechi Anya <ikechi@agures.com>
2024-10-18 19:26:21 +0100

    task/map pointers 1

--- best_routes/get_routes/utils.py
+++ best_routes/get_routes/utils.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import os
 
 def get_nodes(source, destination):
   # Service - 'route', mode of transportation - 'driving', without alternatives
@@ -20,6 +22,29 @@ def get_nodes(source, destination):
 
   return None
 
+def get_fuelup_points(coordinates):
+  gallonDistance = 10
+  # max * liters in a gallon
+  miles_per_litre = 10 / 3.785
+  miles_per_node = 0.3
+  fuel_min_limit = 150
+  points = []
+  current_fuel_in_tank = 0
+  max_fuel_in_tank = 500
+
+  for i in range(0, len(coordinates)):
+    if (current_fuel_in_tank <= fuel_min_limit):
+      points.append({
+        'coordinates': coordinates[i]
+      })
+      current_fuel_in_tank = max_fuel_in_tank
+    else:
+      current_fuel_in_tank = current_fuel_in_tank - (miles_per_node * miles_per_litre)
+
+  # print(len(coordinates), len(points), points, len(coordinates) * 0.3)
+  return points
+
+
 def get_coordinates(nodes):
   url = 'https://overpass-api.de/api/interpreter'
   route_nodes = None
@@ -30,7 +55,7 @@ def get_coordinates(nodes):
     def add_api_string(string, node):
       return string + 'node({});\n'.format(node)
 
-    if (len(nodes) <= 200):
+    if (len(nodes) <= 3):
       for node_ in nodes:
         urlBody = add_api_string(urlBody, node)
     else:
@@ -40,7 +65,7 @@ def get_coordinates(nodes):
 
     urlBody = urlBody + ');\n(._;>;);\nout;'
     res = requests.post(url, urlBody)
-    print("Calling API 2 ...:", res.status_code)
+    print("Calling API 2 ...:", res.status_code, res.json())
     result = res.json()
 
     if ('elements' in result):
@@ -49,3 +74,30 @@ def get_coordinates(nodes):
     return None
   except:
     return None
+
+def get_route(source, destination):
+  if (not source or not destination):
+    return { 'isError': 'Source or destination missing' }
+
+  file = open('get_routes/mock_coordinates.json', 'r')
+  coordinates_src = json.load(file)['elements']
+
+  if (os.getenv('ENVIRONMENT') == 'production'):
+    nodes = get_nodes(source, destination)
+    coordinates_src = get_coordinates(nodes)
+
+  if (not coordinates_src):
+    return { 'isError': 'No route data found' }
+  
+  coordinates = []
+
+  for entry in coordinates_src:
+    coordinates.append([entry['lon'], entry['lat']])
+
+  coordinates.sort(key=lambda coordinates: coordinates[0])
+  context = {
+    'coordinates': coordinates,
+    'fuelup_points': get_fuelup_points(coordinates)
+  }
+  
+  return context
--- best_routes/get_routes/views.py
+++ best_routes/get_routes/views.py
@@ -1,38 +1,17 @@
 from django.shortcuts import render, HttpResponse
-from .utils import get_nodes, get_coordinates
+from .utils import get_nodes, get_coordinates, get_route
 from rest_framework.decorators import api_view
 import json
 import os
-
-def get_coordinates_call(source, destination):
-  if (not source or not destination):
-    return { 'isError': 'Source or destination missing' }
-
-  file = open('get_routes/mock_coordinates.json', 'r')
-  coordinates_src = json.load(file)['elements']
-
-  if (os.getenv('ENVIRONMENT') == 'production'):
-    nodes = get_nodes(source, destination)
-    coordinates_src = get_coordinates(nodes)
-
-  if (not coordinates_src):
-    return { 'isError': 'No route data found' }
-
-  coordinates = { 'body': [] }
-
-  for entry in coordinates_src:
-    coordinates['body'].append([entry['lon'], entry['lat']])
-  
-  return coordinates
 
 @api_view(['POST'])
 def get_routes(request):
   body = json.loads(request.body)
   source, destination = body.values()
-  coordinates_result = get_coordinates_call(source, destination)
+  route = get_route(source, destination)
 
-  if (coordinates_result['isError']):
-    return HttpResponse(coordinates_result['isError'])
+  if (route['isError']):
+    return HttpResponse(route['isError'])
   else:
     return HttpResponse('success')
 
@@ -44,9 +23,9 @@ def get_map(request):
   # javascript var to const
   # r = get_nodes((-80, 35), (-70, 40))
   source, destination = [request.GET.get('source'), request.GET.get('destination')]
-  coordinates_result = get_coordinates_call(source, destination)
+  route = get_route(source, destination)
 
-  if ('isError' in coordinates_result):
-    return HttpResponse(coordinates_result['isError'])
+  if ('isError' in route):
+    return HttpResponse(route['isError'])
   else:
-    return render(request, 'map.html', coordinates_result)
+    return render(request, 'map.html', route)
