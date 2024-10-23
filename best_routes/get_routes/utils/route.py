c7eec8c83e6f4279ea10144c90a645d08f3e9669
Ikechi Anya <ikechi@agures.com>
2024-10-23 16:03:29 +0100

    task/remove unused code

--- best_routes/get_routes/utils/route.py
+++ best_routes/get_routes/utils/route.py
@@ -1,41 +1,8 @@
-from operator import itemgetter
-import requests
 import json
 import os
 
 from get_routes.utils.coordinates import get_route_from_azure
-from get_routes.utils.nodes import get_nodes, node_interval
 from get_routes.utils.logistics import get_logistics
-
-def get_nodes_overpass(nodes):
-  # This api converts all nodes to lng lat
-  url = 'https://overpass-api.de/api/interpreter'
-
-  try:
-    urlBody = 'data=\n[out: json]\n;\n(\n'
-
-    def add_api_string(string, node):
-      return string + 'node({});\n'.format(node)
-
-    if (len(nodes) <= node_interval):
-      for node_ in nodes:
-        urlBody = add_api_string(urlBody, node_)
-    else:
-      for i in range(0, len(nodes)):
-        if (i % node_interval == 1):
-          urlBody = add_api_string(urlBody, nodes[i])
-
-    urlBody = urlBody + ');\n(._;>;);\nout;'
-    res = requests.post(url, urlBody)
-    print("Calling API Overpass ...:", res.status_code)
-    result = res.json()
-
-    if ('elements' in result):
-      return result['elements']
-
-    return None
-  except:
-    return None
 
 def get_route(source, destination):
   if (not source or not destination):
